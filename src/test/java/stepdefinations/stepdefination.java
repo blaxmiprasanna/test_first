ae62d5d03ac0de8dd492a916afb374fdaf765731
Prasanna <blaxmiprasanna@yahoo.com>
2021-12-18 00:02:21 -0500

    change in step defination

--- src/test/java/stepdefinations/stepdefination.java
+++ src/test/java/stepdefinations/stepdefination.java
@@ -1,7 +1,6 @@
 package stepdefinations;
 
-import org.apache.commons.logging.impl.Log4JLogger;
-
+import com.relevantcodes.extentreports.LogStatus;
 import com.tiaa.srktesting.app.BaseClass;
 
 import cucumber.api.java.en.Given;
@@ -10,28 +9,29 @@ import cucumber.api.java.en.When;
 
 public class stepdefination extends BaseClass {
 
-	Log4JLogger log = new Log4JLogger();
+	com.relevantcodes.extentreports.ExtentReports report;
+	com.relevantcodes.extentreports.ExtentTest logger;
 
 	@Given("^User is in home page$")
 	public void user_is_in_home_page() throws Throwable {
-
-		log.info("testing 1");
-
+		report = new com.relevantcodes.extentreports.ExtentReports("./repors/login.html");
+		logger = report.startTest("start the test");
 		setUp();
 	}
 
 	@When("^Enter username and password and click on login button$")
 	public void enter_username_and_password_and_click_on_login_button() throws Throwable {
 		entercredentials();
+		logger.log(LogStatus.INFO, "enter username and password");
 		Thread.sleep(6000);
-		log.info("testing 2");
 	}
 
 	@Then("^user login page should be displayed$")
 	public void user_login_page_should_be_displayed() throws Throwable {
 		teardown();
-		log.info("testing 3");
-
+		logger.log(LogStatus.INFO, "close the browser");
+		report.endTest(logger);
+		report.flush();
 	}
 
 }
