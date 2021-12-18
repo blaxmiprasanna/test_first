f2d0dcf620c60d50cfbcf0c837770d2269410621
Prasanna <blaxmiprasanna@yahoo.com>
2021-12-17 20:03:24 -0500

    log added

--- src/test/java/stepdefinations/stepdefination.java
+++ src/test/java/stepdefinations/stepdefination.java
@@ -1,5 +1,7 @@
 package stepdefinations;
 
+import org.apache.commons.logging.impl.Log4JLogger;
+
 import com.tiaa.srktesting.app.BaseClass;
 
 import cucumber.api.java.en.Given;
@@ -8,8 +10,13 @@ import cucumber.api.java.en.When;
 
 public class stepdefination extends BaseClass {
 
+	Log4JLogger log = new Log4JLogger();
+
 	@Given("^User is in home page$")
 	public void user_is_in_home_page() throws Throwable {
+
+		log.info("testing 1");
+
 		setUp();
 	}
 
@@ -17,11 +24,13 @@ public class stepdefination extends BaseClass {
 	public void enter_username_and_password_and_click_on_login_button() throws Throwable {
 		entercredentials();
 		Thread.sleep(6000);
+		log.info("testing 2");
 	}
 
 	@Then("^user login page should be displayed$")
 	public void user_login_page_should_be_displayed() throws Throwable {
 		teardown();
+		log.info("testing 3");
 
 	}
 
